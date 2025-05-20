4d2cf9cae6d60f5dcdaf56fd843a9491b4694110
Andreas Ehrles <a.ehrles@oth-aw.de>
2025-05-20 18:44:41 +0200

    Grundlegende CRUD Operationen für Getränke zur API hinzugefügt

--- sys-src/GetraenkeIO-backend/app/api/main.py
+++ sys-src/GetraenkeIO-backend/app/api/main.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from .routes import users
+from .routes import users, drinks
 
 # Die Router aller Routen Zentral registrieren
 api_router = APIRouter()
 api_router.include_router(users.router)
+api_router.include_router(drinks.router)
