4915fde17f47d73b5885ae9c2e8a6db7aa1c6f4c
Andreas Ehrles <a.ehrles@oth-aw.de>
2025-05-21 17:52:34 +0200

    Kommentare zur Getränkeschnittstelle der API hinzugefügt

--- sys-src/GetraenkeIO-backend/app/models/drinks.py
+++ sys-src/GetraenkeIO-backend/app/models/drinks.py
@@ -5,21 +5,26 @@ import uuid
 from pydantic import AfterValidator, constr
 from sqlmodel import Field, SQLModel
 
+# Datentyp für Getränkenamen, sodass keine Unsichtbaren Getränkenamen möglich sind
 DrinkName = Annotated[
     constr(min_length=1, pattern=r"^\w(.*\w)?$"),
     Field(index=True, unique=True, schema_extra={"examples": ["Augustiner Lagerbier Hell"]}),
 ]
 
+# Datenmodell für das ändern einzelner Attribute
+# Wenn kein Wert oder None angegeben ist bleibt der aktuelle Wert erhalten
 class DrinkPut(SQLModel):
     name: Optional[DrinkName] = None
     count: Optional[int] = None
     cost: Optional[Decimal] = None
 
+# Datenmodell für das Erstellen eines Neuen Getränkeeintrags
 class DrinkPost(SQLModel):
     name: DrinkName
     count: int
     cost: Decimal
 
+# Tabelle für Getränke
 class Drink(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     name: DrinkName
