79ecc8b4f37bba60851d46ddca398e3d5087af8c
Andreas Ehrles <a.ehrles@oth-aw.de>
2025-05-23 13:11:31 +0200

    Hinzufügen der Bild-Url Eigenschaft zur Getränketabelle

--- sys-src/GetraenkeIO-backend/app/models/drinks.py
+++ sys-src/GetraenkeIO-backend/app/models/drinks.py
@@ -17,7 +17,7 @@ def check_invalid_drink_cost(value: Decimal) -> Decimal:
 # Datentyp für Getränkenamen, sodass keine Unsichtbaren Getränkenamen möglich sind
 DrinkName = Annotated[
     constr(min_length=1, pattern=r"^\w(.*\w)?$"),
-    Field(index=True, unique=True, schema_extra={"examples": ["Augustiner Lagerbier Hell"]}),
+    Field(index=True, unique=False, schema_extra={"examples": ["Augustiner Lagerbier Hell"]}),
 ]
 
 DrinkCount = Annotated[
@@ -32,16 +32,24 @@ DrinkCost = Annotated[
     AfterValidator(check_invalid_drink_cost)
 ]
 
+# Regex von https://uibakery.io/regex-library/url abgerufen am 23.05.2025 um 13:00
+DrinkImageUrl = Annotated[
+    constr(pattern=r"^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$"),
+    Field(index=False, schema_extra={"examples": ["https://cdn.pixabay.com/photo/2017/06/24/23/44/beer-2439238_1280.jpg"]})
+]
+
 # Datenmodell für das ändern einzelner Attribute
 # Wenn kein Wert oder None angegeben ist bleibt der aktuelle Wert erhalten
 class DrinkPut(SQLModel):
     name: Optional[DrinkName] = None
+    imageUrl: Optional[DrinkImageUrl] = None
     count: Optional[DrinkCount] = None
     cost: Optional[DrinkCost] = None
 
 # Datenmodell für das Erstellen eines Neuen Getränkeeintrags
 class DrinkPost(SQLModel):
     name: DrinkName
+    imageUrl: DrinkImageUrl
     count: DrinkCount
     cost: DrinkCost
 
@@ -49,5 +57,6 @@ class DrinkPost(SQLModel):
 class Drink(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     name: DrinkName
+    imageUrl: DrinkImageUrl
     count: DrinkCount
     cost: DrinkCost
