1f2ca04566050b79532ed0f5826ead7ed9953e87
Andreas Ehrles <a.ehrles@oth-aw.de>
2025-05-23 13:46:48 +0200

    Authentifizierung für Admin-User zu Drink-Endpunkt hinzugefügt; Fehler bei Admin-Verifizierung Exception Nachricht behoben

--- sys-src/GetraenkeIO-backend/app/api/routes/drinks.py
+++ sys-src/GetraenkeIO-backend/app/api/routes/drinks.py
@@ -1,6 +1,6 @@
 import uuid
 from fastapi import APIRouter
-from app.api.dependencies import SessionDep
+from app.api.dependencies import CurrentAdminUserDep, SessionDep
 from app.crud.drinks import read_drink_by_id, read_drinks_from_db, store_drink_to_db,delete_drink_by_id, update_drink_by_id
 from app.models.drinks import Drink, DrinkPost, DrinkPut
 
@@ -18,16 +18,16 @@ def read_drink(session:SessionDep, drink_id: uuid.UUID):
     return drink
 
 @router.post("/", response_model=Drink)
-def create_drink(session: SessionDep, drink: DrinkPost):
+def create_drink(session: SessionDep, drink: DrinkPost, user: CurrentAdminUserDep):
     drink = store_drink_to_db(session=session, drink_post=drink)
     return drink
 
 @router.put("/{drink_id}", response_model=Drink)
-def update_drink(session: SessionDep, drink_id: uuid.UUID, drink_put: DrinkPut):
+def update_drink(session: SessionDep, drink_id: uuid.UUID, drink_put: DrinkPut, user: CurrentAdminUserDep):
     drink = update_drink_by_id(session=session, drink_id=drink_id, drink_update=drink_put)
     return drink
 
 @router.delete("/{drink_id}", response_model=Drink)
-def delete_drink(session:SessionDep, drink_id: uuid.UUID):
+def delete_drink(session:SessionDep, drink_id: uuid.UUID, user: CurrentAdminUserDep):
     drink = delete_drink_by_id(session=session, drink_id=drink_id)
     return drink
