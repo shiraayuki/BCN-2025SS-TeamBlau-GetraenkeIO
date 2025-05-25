0e0a4bf95da9bbe44492b712c9b930866012edb3
Justin Weinhut <j.weinhut@oth-aw.de>
2025-05-25 18:03:34 +0200

    History-View added

--- sys-src/GetraenkeIO-frontend/src/App.tsx
+++ sys-src/GetraenkeIO-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import History from './views/History';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/history' element={<History />} />
       </Routes>
     </Router>
   );
