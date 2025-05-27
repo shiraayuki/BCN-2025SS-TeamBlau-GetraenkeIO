a42ab7683353823bc73da4c6641d7ada6f65cde5
Andreas Ehrles <a.ehrles@oth-aw.de>
2025-05-27 19:44:52 +0200

    Zusätzliche Unit-Tests für Get Anfragen an Getränke-Endpunkt hinzugefügt und Authentifizierung zu Get-Request-Schnittstellen hinzugefügt

--- sys-src/GetraenkeIO-backend/app/api/routes/drinks.py
+++ sys-src/GetraenkeIO-backend/app/api/routes/drinks.py
@@ -1,6 +1,6 @@
 import uuid
 from fastapi import APIRouter
-from app.api.dependencies import CurrentAdminUserDep, SessionDep
+from app.api.dependencies import CurrentAdminUserDep, SessionDep, CurrentUserDep
 from app.crud.drinks import read_drink_by_id, read_drinks_from_db, store_drink_to_db,delete_drink_by_id, update_drink_by_id
 from app.models.drinks import Drink, DrinkPost, DrinkPut
 
@@ -8,12 +8,12 @@ from app.models.drinks import Drink, DrinkPost, DrinkPut
 router = APIRouter(prefix="/drinks", tags=["drinks"])
 
 @router.get("/", response_model=list[Drink])
-def read_drinks(session: SessionDep):
+def read_drinks(session: SessionDep, user: CurrentUserDep):
     drinks = read_drinks_from_db(session=session)
     return drinks
 
 @router.get("/{drink_id}", response_model=Drink)
-def read_drink(session:SessionDep, drink_id: uuid.UUID):
+def read_drink(session:SessionDep, drink_id: uuid.UUID, user: CurrentUserDep):
     drink = read_drink_by_id(session=session,drink_id=drink_id)
     return drink
 
--- sys-src/GetraenkeIO-backend/app/tests/api/test_drinks.py
+++ sys-src/GetraenkeIO-backend/app/tests/api/test_drinks.py
@@ -1,4 +1,5 @@
 from fastapi import status
+from sqlmodel import SQLModel, select
 from app.models.user import UserGet, User
 from app.models.drinks import Drink, DrinkPost
 from uuid import uuid4
@@ -18,7 +19,7 @@ VALID_DRINK = Drink(
     count=5,
     cost=3.5)
 
-@pytest.fixture()
+@pytest.fixture(scope="function")
 def setup_normal(db, client):
     VALID_USER = User(
         name = VALID_USER_NAME,
@@ -26,10 +27,10 @@ def setup_normal(db, client):
         guthaben=25.30,
         hashed_password=get_password_hash(VALID_USER_PASSWORD)
         )
-    db.add(VALID_USER)
+    db.add(User.model_validate(VALID_USER))
     db.commit()
 
-@pytest.fixture()
+@pytest.fixture(scope="function")
 def setup_admin(db, client):
     VALID_USER = User(
         name = VALID_USER_NAME,
@@ -37,21 +38,42 @@ def setup_admin(db, client):
         guthaben=25.30,
         hashed_password=get_password_hash(VALID_USER_PASSWORD)
         )
-    db.add(VALID_USER)
+    db.add(User.model_validate(VALID_USER))
     db.commit()
 
-@pytest.fixture()
+@pytest.fixture(scope="function")
 def add_valid_drink(db,client):
-    db.add(VALID_DRINK)
+    db.add(Drink.model_validate(VALID_DRINK))
     db.commit()
 
 def test_drinks_get(db, client,setup_normal,add_valid_drink):
     response = client.get("/drinks", auth=(VALID_USER_NAME, VALID_USER_PASSWORD))
+    assert response.status_code == status.HTTP_200_OK
+    assert type(response.json()) == list
+    assert len(response.json()) == 1
     response_drink = Drink.model_validate(response.json()[0])
     db_drink = read_drink_by_id(session = db,drink_id=VALID_DRINK_UUID)
 
-    assert response.status_code == status.HTTP_200_OK
     assert response_drink.name == VALID_DRINK_NAME == db_drink.name
     assert response_drink.imageUrl == VALID_DRINK.imageUrl == db_drink.imageUrl
     assert response_drink.count == VALID_DRINK.count == db_drink.count
     assert response_drink.cost == VALID_DRINK.cost == db_drink.cost
+
+def test_drinks_get_unauthorized(db, client, setup_normal):
+    response = client.get("/drinks", auth=("WrongUser","WrongPW"))
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+def test_drink_get(db, client,setup_normal,add_valid_drink):
+    response = client.get("/drinks/" + str(VALID_DRINK_UUID), auth=(VALID_USER_NAME, VALID_USER_PASSWORD))
+    assert response.status_code == status.HTTP_200_OK
+    response_drink = Drink.model_validate(response.json())
+    db_drink = read_drink_by_id(session = db,drink_id=VALID_DRINK_UUID)
+
+    assert response_drink.name == VALID_DRINK_NAME == db_drink.name
+    assert response_drink.imageUrl == VALID_DRINK.imageUrl == db_drink.imageUrl
+    assert response_drink.count == VALID_DRINK.count == db_drink.count
+    assert response_drink.cost == VALID_DRINK.cost == db_drink.cost
+
+def test_drink_get_unauthorized(db, client, setup_normal):
+    response = client.get("/drinks/" + str(VALID_DRINK_UUID), auth=("WrongUser","WrongPW"))
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
