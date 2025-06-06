810d4efdaa120f052f89938dc1a28df1d20f3409
Andreas Ehrles <a.ehrles@oth-aw.de>
2025-06-06 14:45:46 +0200

    Vorbereitungsfunktionen zum Einfügen von Daten in die Datenbank für Tests in eigene Datei verschoben

--- sys-src/GetraenkeIO-backend/app/tests/api/test_drinks.py
+++ sys-src/GetraenkeIO-backend/app/tests/api/test_drinks.py
@@ -6,45 +6,7 @@ from uuid import uuid4
 from app.core.security import get_password_hash
 from app.crud.drinks import read_drink_by_id
 import pytest
-
-VALID_USER_NAME = "TestBenutzer"
-VALID_USER_PASSWORD = "FakeHashedPassword"
-
-VALID_DRINK_NAME = "TestGetraenk"
-VALID_DRINK_UUID = uuid4()
-VALID_DRINK = Drink(
-    id=VALID_DRINK_UUID,
-    name=VALID_DRINK_NAME,
-    imageUrl="https://images.pexels.com/photos/28220413/pexels-photo-28220413/free-photo-of-flaschengrun.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    count=5,
-    cost=3.5)
-
-@pytest.fixture(scope="function")
-def setup_normal(db, client):
-    VALID_USER = User(
-        name = VALID_USER_NAME,
-        is_admin=False,
-        guthaben=25.30,
-        hashed_password=get_password_hash(VALID_USER_PASSWORD)
-        )
-    db.add(User.model_validate(VALID_USER))
-    db.commit()
-
-@pytest.fixture(scope="function")
-def setup_admin(db, client):
-    VALID_USER = User(
-        name = VALID_USER_NAME,
-        is_admin=True,
-        guthaben=25.30,
-        hashed_password=get_password_hash(VALID_USER_PASSWORD)
-        )
-    db.add(User.model_validate(VALID_USER))
-    db.commit()
-
-@pytest.fixture(scope="function")
-def add_valid_drink(db,client):
-    db.add(Drink.model_validate(VALID_DRINK))
-    db.commit()
+from app.tests.setup_data import add_valid_drink, setup_admin, setup_normal, VALID_DRINK, VALID_DRINK_NAME, VALID_DRINK_UUID, VALID_USER_NAME, VALID_USER_PASSWORD
 
 def test_drinks_get(db, client,setup_normal,add_valid_drink):
     response = client.get("/drinks", auth=(VALID_USER_NAME, VALID_USER_PASSWORD))
---
+++ sys-src/GetraenkeIO-backend/app/tests/setup_data.py
@@ -0,0 +1,45 @@
+from uuid import uuid4
+import pytest
+from app.core.security import get_password_hash
+from app.models.drinks import Drink
+from app.models.user import User
+
+
+VALID_USER_NAME = "TestBenutzer"
+VALID_USER_PASSWORD = "FakeHashedPassword"
+
+VALID_DRINK_NAME = "TestGetraenk"
+VALID_DRINK_UUID = uuid4()
+VALID_DRINK = Drink(
+    id=VALID_DRINK_UUID,
+    name=VALID_DRINK_NAME,
+    imageUrl="https://images.pexels.com/photos/28220413/pexels-photo-28220413/free-photo-of-flaschengrun.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    count=5,
+    cost=3.5)
+
+@pytest.fixture(scope="function")
+def setup_normal(db, client):
+    VALID_USER = User(
+        name = VALID_USER_NAME,
+        is_admin=False,
+        guthaben=25.30,
+        hashed_password=get_password_hash(VALID_USER_PASSWORD)
+        )
+    db.add(User.model_validate(VALID_USER))
+    db.commit()
+
+@pytest.fixture(scope="function")
+def setup_admin(db, client):
+    VALID_USER = User(
+        name = VALID_USER_NAME,
+        is_admin=True,
+        guthaben=25.30,
+        hashed_password=get_password_hash(VALID_USER_PASSWORD)
+        )
+    db.add(User.model_validate(VALID_USER))
+    db.commit()
+
+@pytest.fixture(scope="function")
+def add_valid_drink(db,client):
+    db.add(Drink.model_validate(VALID_DRINK))
+    db.commit()
