71b22c77691bec581e89af5d53cd78ef4d80bc14
Andreas Ehrles <a.ehrles@oth-aw.de>
2025-06-16 16:04:22 +0200

    Regex für Getränke URL ausgetauscht, um mehr URLs zuzulassen

--- sys-src/GetraenkeIO-backend/app/models/drinks.py
+++ sys-src/GetraenkeIO-backend/app/models/drinks.py
@@ -7,7 +7,7 @@ from sqlmodel import Field, SQLModel
 
 def check_invalid_drink_counter(value: int) -> int:
     if value < 0:
-        raise ValueError(f'Der Wert des Attributs cout darf nicht kleiner als 0 sein!')
+        raise ValueError(f'Der Wert des Attributs count darf nicht kleiner als 0 sein!')
     return value
 def check_invalid_drink_cost(value: Decimal) -> Decimal:
     if value < 0:
@@ -32,9 +32,9 @@ DrinkCost = Annotated[
     AfterValidator(check_invalid_drink_cost)
 ]
 
-# Regex von https://uibakery.io/regex-library/url abgerufen am 23.05.2025 um 13:00
+# Regex von https://dev.to/jbxamora/url-regex-1pol abgerufen am 16.06.2025 um 15:30
 DrinkImageUrl = Annotated[
-    constr(pattern=r"^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$"),
+    constr(pattern=r"^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$"),
     Field(index=False, schema_extra={"examples": ["https://cdn.pixabay.com/photo/2017/06/24/23/44/beer-2439238_1280.jpg"]})
 ]
 
